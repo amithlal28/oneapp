7cb68201675af193fc25dc25cc59d7e24c2c47d4
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2023-03-05 16:07:39 +0530

    Update store.dart

---
+++ lib/screens/store.dart
@@ -0,0 +1,55 @@
+
+
+import 'package:flutter/material.dart';
+
+import '../backend/auth.dart';
+import '../sizes.dart';
+
+class Store extends StatefulWidget {
+  const Store({Key? key}) : super(key: key);
+
+  @override
+  State<Store> createState() => _StoreState();
+}
+
+class _StoreState extends State<Store> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff2a2a2a),
+      appBar: AppBar(
+        backgroundColor: const Color(0xff2a2a2a),
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            height: 40,
+            width: 40,
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            clipBehavior: Clip.antiAlias,
+            child: Image.network(user!.photoURL!),
+          )
+        ],
+      ),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+              horizontal: 20, vertical:  screenHeight(context, mulBy: 0.03)),
+          children:[],
+        ),
+      ),
+
+    );
+  }
+}
